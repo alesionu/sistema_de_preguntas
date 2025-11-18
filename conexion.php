7e7a91a976ff14227e85b09b6cb152472dcd37c0
Santiago Almada <santiiverde01@gmail.com>
2025-11-17 22:49:52 -0300

    Nuevo cambio

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 
 $conexion = mysqli_connect(
-  'localhost', 'root', '', 'bd_gestion_examenes1');
+  'localhost', 'root', '', 'bd_gestion_examenes');
   
   if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
